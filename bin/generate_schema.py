9407846affc65a9a1ecd6a396c8965f71e1faf0f
santacana.canton <santacana.canton@icloud.com>
2024-10-25 09:32:44 +0200

    New: nueva version del generador de esquemas en python

---
+++ bin/generate_schema.py
@@ -0,0 +1,32 @@
+import sys
+import json
+import os
+from jsonschema import Draft7Validator
+
+def generate_schema(markdown_file, output_file):
+    # Aquí puedes definir cómo quieres generar el esquema a partir del archivo Markdown
+    # Este es un ejemplo básico que crea un esquema vacío
+    schema = {
+        "$schema": "http://json-schema.org/draft-07/schema#",
+        "title": os.path.basename(markdown_file),
+        "type": "object",
+        "properties": {
+            "content": {
+                "type": "string"
+            }
+        },
+        "required": ["content"]
+    }
+
+    with open(output_file, 'w') as f:
+        json.dump(schema, f, indent=2)
+
+if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        print("Usage: generate_schema.py <input_markdown_file> <output_json_file>")
+        sys.exit(1)
+
+    input_markdown_file = sys.argv[1]
+    output_json_file = sys.argv[2]
+
+    generate_schema(input_markdown_file, output_json_file)
